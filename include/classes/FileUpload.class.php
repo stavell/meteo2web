401203af81e20f2155cf8c4de25a9af327182aa9
svelchev@Gmail.com <Marmalad23>
2014-01-21 23:52:23 +0200

    bla mla

--- include/classes/FileUpload.class.php
+++ include/classes/FileUpload.class.php
@@ -29,9 +29,6 @@ class FileUpload {
             urlencode($sFileName)
         );
 
-
-        \DB::startTransaction();
-
         $handle = fopen($sUrl, "w");
         if(!$handle) throw new XCException('Can not open file device');
 
@@ -47,7 +44,6 @@ class FileUpload {
         \DB::insert('files', array($aFile));
         $aFile['id'] = \DB::insertId();
 
-        \DB::commit();
 
         return $aFile;
     }
