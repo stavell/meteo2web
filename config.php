df6f17121e528c3e0ef27334bbe887816c8ac9e8
svelchev@Gmail.com <Marmalad23>
2014-03-06 19:48:13 +0200

    da

--- config.php
+++ config.php
@@ -3,4 +3,4 @@
 define('METEO2_DB_HOST', 'stavl.com');
 define('METEO2_DB_USER', 'meteo2');
 define('METEO2_DB_PASS', 'meteo2@pass');
-define('METEO2_DB_DB', 'shumenxc_meteo');
+define('METEO2_DB_DB', 'meteo');
