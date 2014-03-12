c2a61410ccc19bcaf42378a5c08ed85c34098faf
Stavel <stavel@it-partner.bg>
2014-03-12 09:46:47 +0200

    das

--- include/classes/Meteo2.class.php
+++ include/classes/Meteo2.class.php
@@ -80,7 +80,7 @@ class Meteo2 {
 
             $oFileUpload = new FileUpload();
             $oFileUpload->uploadFile(
-                MD5(file_get_contents($_FILES["file"]["tmp_name"])).'.'.pathinfo($_FILES['file']['name'], PATHINFO_EXTENSION),
+                sha1(file_get_contents($_FILES["file"]["tmp_name"])).'.'.pathinfo($_FILES['file']['name'], PATHINFO_EXTENSION),
                 $_FILES["file"]["tmp_name"]
             );
 
