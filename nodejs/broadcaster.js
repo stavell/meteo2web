de7b196101ff64076a603cf801f6335b9e1f010d
svelchev@Gmail.com <Marmalad23>
2014-03-24 22:05:23 +0200

    broadcasteri nekvi

--- nodejs/broadcaster.js
+++ nodejs/broadcaster.js
@@ -1,11 +1,20 @@
-var webSocketServer = new require('ws').Server({port: 10080});
+var BroadcastClients = {};
 
-var webServer = require('http').createServer(function (req, res) {
+webSocketServer = new require('ws').Server;
+var wss = new webSocketServer({port: 10080});
 
+wss.on('connection', function(ws) {
+    ws.on('message', function(message) {
+        BroadcastClients[message] =  BroadcastClients[message] || [];
+        BroadcastClients[message].push(this);
+        ws.send("Registered as user "+BroadcastClients[message].length+" for ["+message+"]");
+    });
+});
+
+var webServer = new require('http').createServer(function (req, res) {
     if(res.socket.remoteAddress == '127.0.0.1' && req.method == 'POST') {
 
         var fullBody = '';
-
         req.on('data', function(chunk) {
             fullBody += chunk.toString();
         });
@@ -14,17 +23,17 @@ var webServer = require('http').createServer(function (req, res) {
             res.writeHead(200, "OK", {'Content-Type': 'text/html'});
             res.end();
 
-
-            notifyAll(fullBody);
+            processNotification(req, fullBody);
         });
-
     }
+});
 
-}).listen(8001);
+webServer.listen(8001);
 
-
-
-var notifyAll = function(req){
-    for(var i in wss.clients) wss.clients[i].send(req);
-    console.log('clients count:'+wss.clients.length)
+var processNotification = function(request, data) {
+    for(var group in BroadcastClients){
+        for(var client in BroadcastClients[group]) {
+            BroadcastClients[group][client].send(data);
+        }
+    }
 }
