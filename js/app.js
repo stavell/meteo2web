2adaf7fdb1fdce0372452d9ab2ea4fcf3d7896a9
svelchev@Gmail.com <Marmalad23>
2014-05-19 00:46:03 +0300

    dad

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+BASE_URL = '';
+
 var App = {};
 
 App.timeParams = {
@@ -40,4 +42,52 @@ App.getFormatedTime = function(date) {
     return (date.getHours()< 9 ? '0':'')+date.getHours() + ':' + (date.getMinutes()< 10 ? '0':'') + date.getMinutes();
 };
 
+
+
+
 App.isMobileBrowser =  /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+
+
+App.initCameraViewer = function(obj, params) {
+
+    $(obj).each(function(){
+        var el = $(this)[0];
+        if(!el) return;
+
+        el.fileIndex = 0;
+
+        el.onImageChanged = params.onImageChanged || function(){};
+
+        el.setFiles = function(files) {
+            el.files = files;
+            el.fileIndex = 0;
+            el.setImageFromIndex(el.fileIndex);
+        };
+
+        el.showNext = function() {
+            if(!el.getFileByIndex(el.fileIndex+1)) return;
+            el.fileIndex++;
+            return el.setImageFromIndex(el.fileIndex);
+        };
+
+        el.showPrev = function() {
+            if(!el.getFileByIndex(el.fileIndex-1)) return;
+            el.fileIndex--;
+            return el.setImageFromIndex(el.fileIndex);
+        };
+
+        el.setImageFromIndex = function(idx) {
+            var file = el.getFileByIndex(idx);
+            if(!file) return false;
+            $(el).attr({src:file.url});
+            el.onImageChanged.apply(el,[file]);
+            return file;
+        }
+
+        el.getFileByIndex = function(idx) {
+            if(el.files[idx]) return el.files[idx];
+            return false;
+        }
+    });
+
+};
