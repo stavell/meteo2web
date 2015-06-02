926f1a1a86285fb0d7aa2927e0e5c0804f712915
Stavel <stavel@it-partner.bg>
2015-06-02 15:54:50 +0300

    ds

---
+++ monitor.php
@@ -0,0 +1,10 @@
+<?php
+use shumenxc as xc;
+require_once('config.inc.php');
+
+$nLastPhotoTime = strtotime(DB::queryFirstField('SELECT created_time FROM files ORDER BY created_time desc limit 1'));
+$nLastDataTime = strtotime(DB::queryFirstField('SELECT created_time FROM data ORDER BY created_time desc limit 1'));
+
+if($nLastDataTime < strtotime("-5 min") || $nLastPhotoTime < strtotime("-15 min") ) {
+    mail("svelchev@gmail.com, stavel@icloud.com", "Shumen-XC Meteo Alert", "Last photo: ".date("Y-m-d H:i:s",$nLastPhotoTime)."\n"."Last data: ".date("Y-m-d H:i:s",$nLastDataTime));
+}
