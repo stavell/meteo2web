9e322f4823a8878292789f2685610ee7c6390178
Stavel <stavel@it-partner.bg>
2015-07-10 16:45:25 +0300

    ds

--- monitor.php
+++ monitor.php
@@ -11,7 +11,7 @@ $diskUsagePercentage = intval(reset($matches));
 
 
 if($nLastDataTime < strtotime("-5 min") || $nLastPhotoTime < strtotime("-15 min") || $diskUsagePercentage >= 90) {
-    mail("svelchev@gmail.com, stavel@icloud.com, zlati.dimitrov@gmail.com", "Shumen-XC Meteo Alert", "Disk usage: ".$diskUsagePercentage."%"."\n"."Last photo: ".date("Y-m-d H:i:s",$nLastPhotoTime)."\n"."Last data: ".date("Y-m-d H:i:s",$nLastDataTime)."\n".'<a href="http://stavl.com/meteo2/smsReboot.php">Изпрати SMS рестарт</a>');
+    mail("svelchev@gmail.com, stavel@icloud.com, zlati.dimitrov@gmail.com", "Shumen-XC Meteo Alert", "Disk usage: ".$diskUsagePercentage."%"."\n"."Last photo: ".date("Y-m-d H:i:s",$nLastPhotoTime)."\n"."Last data: ".date("Y-m-d H:i:s",$nLastDataTime)."\n".'http://stavl.com/meteo2/smsReboot.php');
 }
 
 echo date("Y-m-d H:i:s");
