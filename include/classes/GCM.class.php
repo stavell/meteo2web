c491866c39c2eb054c4ce3bf6183e1efde3b3113
stavel <svelchev@gmail.com>
2015-12-21 00:34:35 +0200

    d

--- include/classes/GCM.class.php
+++ include/classes/GCM.class.php
@@ -3,14 +3,21 @@ namespace shumenxc;
 
 class GCM {
 
-    public static function registerDevice($token, $deviceBrand = null, $deviceModel = null, $androidVersion = null) {
-        error_log(var_export(func_get_args(),1));
-        return array(
+    public static function registerDevice($id = null, $token, $deviceBrand = null, $deviceModel = null, $androidVersion = null) {
+        if(empty($token)) throw new XCInvalidParam("no token");
+
+        $device = array(
+            'id' => $id,
             'token' => $token,
             'deviceBrand' => $deviceBrand,
             'deviceModel' => $deviceModel,
             'androidVersion' => $androidVersion
         );
+
+        \DB::insertUpdate('devices', $device);
+        if(empty($id)) $id = \DB::insertId();
+
+        return \DB::queryOneRow("select * from devices where id = $id");
     }
 
 
