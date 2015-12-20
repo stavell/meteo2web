5acbaf74d722b4018a0a6c9f4161c6b05260aed3
stavel <svelchev@gmail.com>
2015-12-21 01:00:33 +0200

    d

--- include/classes/GCM.class.php
+++ include/classes/GCM.class.php
@@ -1,6 +1,8 @@
 <?php
 namespace shumenxc;
 
+use GuzzleHttp\Client;
+
 class GCM {
 
     public static function registerDevice($id = null, $token, $deviceBrand = null, $deviceModel = null, $androidVersion = null) {
@@ -21,6 +23,27 @@ class GCM {
         return \DB::queryOneRow("select * from devices where id = $id");
     }
 
+
+    public static function notifyDevice($id, $message) {
+        $client = new Client();
+
+        $token = \DB::queryFirstField("SELECT token FROM devices WHERE id = $id");
+        if(empty($token)) throw new XCInvalidParam("No device found");
+
+        $response = $client->request('POST', 'https://gcm-http.googleapis.com/gcm/send', [
+            'headers' => [
+                'Authorization' => 'key='.GCM_AUTH
+            ],
+            'json' => [
+                'to' => $token,
+                'data' => [
+                    'message' => $message
+                ]
+            ]
+        ]);
+
+        return json_decode($response->getBody()->getContents());
+    }
 
 
 }
