5aaddfdf66b6e2d20dd6c6b1d25abaa2433d0897
stavel <svelchev@gmail.com>
2015-12-21 00:05:01 +0200

    d

--- include/classes/GCM.class.php
+++ include/classes/GCM.class.php
@@ -3,8 +3,14 @@ namespace shumenxc;
 
 class GCM {
 
-    public static function registerToken($sToken) {
+    public static function registerDevice($token, $deviceBrand = null, $deviceModel = null, $androidVersion = null) {
         error_log(var_export(func_get_args(),1));
+        return array(
+            'token' => $token,
+            'deviceBrand' => $deviceBrand,
+            'deviceModel' => $deviceModel,
+            'androidVersion' => $androidVersion
+        );
     }
 
 
