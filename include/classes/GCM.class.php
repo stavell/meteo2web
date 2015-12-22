5a06c9dc6f0e4264ef318aed3830aebc1ec8a97a
stavel <svelchev@gmail.com>
2015-12-22 23:42:33 +0200

    d

--- include/classes/GCM.class.php
+++ include/classes/GCM.class.php
@@ -24,27 +24,50 @@ class GCM {
     }
 
 
-    public static function notifyDevice($id, $data) {
+    public static function ackMessage($messageID){
+        $message = \DB::queryOneRow("select * from messages where message_id = $messageID");
+        if(empty($messageID)) throw new XCInvalidParam;
+
+        $message['receive_time'] = date('Y-m-d H:i:s');
+        \DB::update('messages', $message);
+
+        return true;
+    }
+
+    public static function notifyDevice($id, $payload) {
         $client = new Client();
+
+        $data['message_id'] = sha1(json_encode($payload).time().microtime(1));
+        $data['payload'] = json_encode($payload, JSON_BIGINT_AS_STRING | JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
 
         $token = \DB::queryFirstField("SELECT token FROM devices WHERE id = $id");
         if(empty($token)) throw new XCInvalidParam("No device found");
 
         /** @noinspection PhpUndefinedConstantInspection */
-        $response = $client->request('POST', 'https://gcm-http.googleapis.com/gcm/send', [
+        $params = [
             'headers' => [
                 'Authorization' => 'key='.GCM_AUTH
             ],
             'json' => [
                 'to' => $token,
                 'priority' => 'high',
-                'collapse_key' => sha1(time()),
                 'data' => $data
             ]
-        ]);
+        ];
 
-        return json_decode($response->getBody()->getContents());
+        $response = $client->request('POST', 'https://gcm-http.googleapis.com/gcm/send', $params);
+
+        $result = json_decode($response->getBody()->getContents(), true);
+
+        if(empty($result['success'])) throw new XCInvalidParam;
+
+        $message['message_id'] = $data['message_id'];
+        $message['device_to'] = $id;
+        $message['message'] = json_encode($params,JSON_BIGINT_AS_STRING | JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
+
+        \DB::insertUpdate('messages', $message);
+
+        return $result;
     }
-
 
 }
