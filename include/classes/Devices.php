fff7b98e71ca0edec6680ec5c2bfc8563efa7129
Stavel <stavel@it-partner.bg>
2016-06-04 19:04:24 +0300

    dev

--- include/classes/Devices.php
+++ include/classes/Devices.php
@@ -3,9 +3,10 @@ namespace shumenxc;
 
 class Devices {
 
-    public function statusUpdate($idDevice, $statusInfo){
+    public function statusUpdate($idDevice, $status, $info = ''){
         $message['device_id'] = $idDevice;
-        $message['status'] = $statusInfo;
+        $message['status'] = $status;
+        $message['info'] = $info;
         \DB::insertUpdate('device_status_events', $message);
     }
 
