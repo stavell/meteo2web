d7b9e8eb253423873af33b9822de5f89d8516c17
Stavel <stavel@it-partner.bg>
2016-06-05 13:47:15 +0300

    dev

--- include/classes/Devices.class.php
+++ include/classes/Devices.class.php
@@ -6,8 +6,8 @@ class Devices {
     public static function statusUpdate($idDevice, $status, $info = ''){
         $message['device_id'] = $idDevice;
         $message['status'] = $status;
-        $message['info'] = $info;
-        \DB::insertUpdate('device_status_events', $message);
+        $message['info'] = empty($info) ? '' : $info;
+        \DB::insert('device_status_events', $message);
     }
 
 }
