c549b8d55a851619a45db1a2b87bcc4b07f53880
stavel <svelchev@gmail.com>
2017-03-23 17:44:06 +0200

    dev

--- include/classes/Devices.class.php
+++ include/classes/Devices.class.php
@@ -6,7 +6,7 @@ class Devices {
     public static function statusUpdate($idDevice, $status, $info = '') {
         $message['device_id'] = $idDevice;
         $message['status'] = $status;
-        $message['info'] = empty($info) ? '' : $info;
+        $message['info'] = json_encode($info, JSON_BIGINT_AS_STRING | JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
         $message['created_time'] = date('Y-m-d H:i:s');
         \DB::insert('device_status_events', $message);
     }
